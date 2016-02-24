d163cecdff53e1c8dc8e0e0bbadcd379a40268d4
nobodyiam <nobodyiam@gmail.com>
2016-02-24 14:13:06 +0800

    use url path name as config name

--- spring-cloud-config-demo-client/src/main/java/com/nobodyiam/spring/cloud/config/demo/client/SomeClientController.java
+++ spring-cloud-config-demo-client/src/main/java/com/nobodyiam/spring/cloud/config/demo/client/SomeClientController.java
@@ -3,6 +3,7 @@ package com.nobodyiam.spring.cloud.config.demo.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,8 @@ public class SomeClientController {
     @Autowired
     private Environment env;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String foo() {
-        return env.getProperty("foo", "undefined");
+    @RequestMapping(value = "/{configName}", method = RequestMethod.GET)
+    public String queryConfig(@PathVariable String configName) {
+        return env.getProperty(configName, "undefined");
     }
 }
