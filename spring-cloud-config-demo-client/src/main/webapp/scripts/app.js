e905d077b2a708424dd3cdbbf51192a6607a613b
nobodyiam <nobodyiam@gmail.com>
2016-02-25 15:43:11 +0800

    add env display

--- spring-cloud-config-demo-client/src/main/webapp/scripts/app.js
+++ spring-cloud-config-demo-client/src/main/webapp/scripts/app.js
@@ -7,6 +7,21 @@
     ]);
 
     app.controller('CloudConfigController', function ($scope, $http, $modal, toastr) {
+        this.env = {};
+
+        var self = this;
+
+        this.loadEnv = function() {
+            $http.get("demo/env")
+                .success(function (data) {
+                    self.env = data;
+                })
+                .error(function (data, status) {
+                    toastr.error((data && data.msg) || 'Loading failed');
+                });
+        };
+
+        this.loadEnv();
 
     });
 
