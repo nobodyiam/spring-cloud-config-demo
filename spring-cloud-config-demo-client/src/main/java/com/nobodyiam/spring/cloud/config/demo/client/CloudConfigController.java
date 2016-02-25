9c118ffca7b36ba980f3b2958bc9414bee832122
nobodyiam <nobodyiam@gmail.com>
2016-02-25 15:27:58 +0800

    add env api

--- spring-cloud-config-demo-client/src/main/java/com/nobodyiam/spring/cloud/config/demo/client/CloudConfigController.java
+++ spring-cloud-config-demo-client/src/main/java/com/nobodyiam/spring/cloud/config/demo/client/CloudConfigController.java
@@ -4,10 +4,7 @@ import com.nobodyiam.spring.cloud.config.demo.model.EnvironmentConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Jason on 2/24/16.
@@ -16,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 @RequestMapping("/demo")
 public class CloudConfigController {
+    private static final String APPLICATION_NAME_CONFIG = "spring.application.name";
+    private static final String ACTIVE_PROFILE_CONFIG = "spring.profiles.active";
+    private static final String LABEL_CONFIG = "spring.cloud.config.label";
+    private static final String CONFIG_SERVER_CONFIG = "spring.cloud.config.uri";
+
     @Autowired
     private Environment env;
 
@@ -25,8 +27,14 @@ public class CloudConfigController {
     }
 
     @RequestMapping(value = "/env", method = RequestMethod.GET)
+    @ResponseBody
     public EnvironmentConfig queryEnv() {
-        return new EnvironmentConfig();
+        return EnvironmentConfig.builder()
+                .application(env.getProperty(APPLICATION_NAME_CONFIG))
+                .profiles(env.getProperty(ACTIVE_PROFILE_CONFIG))
+                .label(env.getProperty(LABEL_CONFIG))
+                .serverUri(env.getProperty(CONFIG_SERVER_CONFIG))
+                .build();
     }
 
 }
--- spring-cloud-config-demo-client/src/main/java/com/nobodyiam/spring/cloud/config/demo/model/EnvironmentConfig.java
+++ spring-cloud-config-demo-client/src/main/java/com/nobodyiam/spring/cloud/config/demo/model/EnvironmentConfig.java
@@ -4,5 +4,67 @@ package com.nobodyiam.spring.cloud.config.demo.model;
  * Created by Jason on 2/25/16.
  */
 public class EnvironmentConfig {
+    private final String application;
+    private final String profiles;
+    private final String label;
+    private final String serverUri;
+
+    public EnvironmentConfig(String application, String profiles, String label, String serverUri) {
+        this.application = application;
+        this.profiles = profiles;
+        this.label = label;
+        this.serverUri = serverUri;
+    }
+
+    public static EnvironmentConfigBuilder builder() {
+        return new EnvironmentConfigBuilder();
+    }
+
+    public String getApplication() {
+        return application;
+    }
+
+    public String getProfiles() {
+        return profiles;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getServerUri() {
+        return serverUri;
+    }
+
+    public static class EnvironmentConfigBuilder {
+        private String application;
+        private String profiles;
+        private String label;
+        private String serverUri;
+
+        public EnvironmentConfigBuilder application(String application) {
+            this.application = application;
+            return this;
+        }
+
+        public EnvironmentConfigBuilder profiles(String profiles) {
+            this.profiles = profiles;
+            return this;
+        }
+
+        public EnvironmentConfigBuilder label(String label) {
+            this.label = label;
+            return this;
+        }
+
+        public EnvironmentConfigBuilder serverUri(String serverUri) {
+            this.serverUri = serverUri;
+            return this;
+        }
+
+        public EnvironmentConfig build() {
+            return new EnvironmentConfig(application,profiles,label,serverUri);
+        }
+    }
 
 }
