4b064d86f36e51526990b464bd624c4098f72367
nobodyiam <nobodyiam@gmail.com>
2016-02-28 11:14:16 +0800

    refactor

---
+++ spring-cloud-config-demo-client/src/main/java/com/nobodyiam/spring/cloud/config/demo/client/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nobodyiam.spring.cloud.config.demo.client;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+}
